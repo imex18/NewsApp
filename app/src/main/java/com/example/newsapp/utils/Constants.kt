d76badf50274859abce2b867298963b1aa55545c
imex18 <35682221+imex18@users.noreply.github.com>
2021-01-30 14:25:47 +0000

    Update Constants.kt

--- app/src/main/java/com/example/newsapp/utils/Constants.kt
+++ app/src/main/java/com/example/newsapp/utils/Constants.kt
@@ -4,7 +4,7 @@ class Constants {
 
     companion object {
 
-      const val API_KEY = "d6b11ec163c945edb21e3deabef3b7c4"
+      const val API_KEY = " PLEASE GET AN API FROM newsapi.com"
         const val BASE_URL = "https://newsapi.org"
     }
 }
